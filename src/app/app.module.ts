ae778f77ce4c62d6b12719f9a0b207de9c97993c
Mahfuz Shuvo <i.maf.shuvo@gmail.com>
2021-07-01 02:26:11 +0600

    navbar menu and menu routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CountriesComponent } from './components/countries/countries.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CountriesComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
